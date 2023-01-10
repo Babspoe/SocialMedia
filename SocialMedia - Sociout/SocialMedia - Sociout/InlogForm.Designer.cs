8111766a1883bb62b277bb4adfc7a31b21deae95
Bas te Koppele <400041421@st.roc.a12.nl>
2023-01-10 14:07:04 +0100

    dbLibrary

--- SocialMedia - Sociout/SocialMedia - Sociout/InlogForm.Designer.cs
+++ SocialMedia - Sociout/SocialMedia - Sociout/InlogForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SocialMedia___Sociout
 {
-    partial class Form1
+    partial class InlogForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNaam = new System.Windows.Forms.TextBox();
+            this.tbWachtwoord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gebruikersnaam:";
+            // 
+            // tbNaam
+            // 
+            this.tbNaam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNaam.Location = new System.Drawing.Point(61, 97);
+            this.tbNaam.Name = "tbNaam";
+            this.tbNaam.Size = new System.Drawing.Size(245, 30);
+            this.tbNaam.TabIndex = 1;
+            // 
+            // tbWachtwoord
+            // 
+            this.tbWachtwoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbWachtwoord.Location = new System.Drawing.Point(61, 172);
+            this.tbWachtwoord.Name = "tbWachtwoord";
+            this.tbWachtwoord.Size = new System.Drawing.Size(245, 30);
+            this.tbWachtwoord.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wachtwoord:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(102, 295);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(157, 33);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignin.Location = new System.Drawing.Point(102, 334);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(157, 33);
+            this.btnSignin.TabIndex = 5;
+            this.btnSignin.Text = "Registreren";
+            this.btnSignin.UseVisualStyleBackColor = true;
+            // 
+            // InlogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.btnSignin);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbWachtwoord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNaam);
+            this.Controls.Add(this.label1);
+            this.Name = "InlogForm";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.InlogForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNaam;
+        private System.Windows.Forms.TextBox tbWachtwoord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnSignin;
     }
 }
 
--- SocialMedia - Sociout/SocialMedia - Sociout/InlogForm.cs
+++ SocialMedia - Sociout/SocialMedia - Sociout/InlogForm.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dbLibrary;
 
 namespace SocialMedia___Sociout
 {
-    public partial class Form1 : Form
+    public partial class InlogForm : Form
     {
-        public Form1()
+        public InlogForm()
         {
             InitializeComponent();
+        }
+
+        dbFunctions db = new dbFunctions();
+        private void InlogForm_Load(object sender, EventArgs e)
+        {
+            if (!db.tryConnection())
+            {
+                MessageBox.Show("Kan geen verbinding maken met de datebase");
+            }
         }
     }
 }
